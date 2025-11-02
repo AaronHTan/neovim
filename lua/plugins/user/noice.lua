5a79ab15f980d1b0d3aecc52d95821215d2e2f24
Aaron <aaron.tan@cryppro.com>
2025-11-02 15:11:19 +0000

    [experimental] experimental changes

--- lua/plugins/user/noice.lua
+++ lua/plugins/user/noice.lua
@@ -4,6 +4,7 @@ return {
   event = "VeryLazy",
   opts = {
     -- add any options here
+    presets = {},
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
